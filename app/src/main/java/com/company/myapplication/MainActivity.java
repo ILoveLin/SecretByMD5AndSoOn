13cf1f4f09f0880555dfa61c07d11fdb40b4e937
tongzhengtong <tongzhengtong@126.com>
2021-12-14 17:19:31 +0800

    update
    更新webserver  访问测试过天气和手机号码公开api ---OK的

--- app/src/main/java/com/company/myapplication/MainActivity.java
+++ app/src/main/java/com/company/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.company.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.company.myapplication.base64.Base64Utils;
+import com.company.myapplication.webservicev2.WebserviceV2Activity;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    private static String PRIVATE_KEY = "私钥";
     private static String PUCLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFa1i3soqRUVgYVg3xhyK48VLnIvVaRhQOwp+8AvgrOjB+PNXYU2iImLvTbyxf452hzvt0q3gDVoMZPZUWjTR6AlvHi5dWDj7HPczaFcG5NEps4X0P6V4JJqZzTzqFBSG9s0dO9qosCjfkRbSbzvNkC2YKjr5tVKPXK7ADd9K1TwIDAQAB";
     private static String PRIVATE_KEY = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIVrWLeyipFRWBhWDfGHIrjxUuci9VpGFA7Cn7wC+Cs6MH481dhTaIiYu9NvLF/jnaHO+3SreANWgxk9lRaNNHoCW8eLl1YOPsc9zNoVwbk0SmzhfQ/pXgkmpnNPOoUFIb2zR072qiwKN+RFtJvO82QLZgqOvm1Uo9crsAN30rVPAgMBAAECgYAo7W0hrNtlCJcFoCBW3yV/pkfQL6EkOCse/AUUH7URTaBimTbgTXXbsT6s9YWfRKuuX1gzygXewEMnEwURDe/yq80zBpKIeXp28xFkcmIP7yfLylTTLHg2tOMRtOGzcTORwU6MspCncTlIUWPlh3/obQY07YdCcbCECjTos7iEAQJBAMj/iTAthwvIrrPH3kqBcpobZxCB19N9dMWE+yLogj3lMh6aBOp4WZo1lek7LgHT1B8gkh0HqhoDoqqGGTmb4w8CQQCp7bqxUi/WUGX98YhTrKLhmSJQG4sx80iF4h/AFzpCt0LimpIORAf/VM0n2wjN+Iv9MwHR032Jo7KV1ND4nwnBAkAkSPVELEPf3bekHuXDP4EnzuCZO0dyF4jYC3ymvaCded3FZnqStW0/iILqmtNgYxQ9Fk0qTnLnxVY9QH4XR0wDAkAIFPt1JZCK4+YF7u/p8uMIUc00/CphpM2FOJ0D+NbXh3nR50IdFZU/ypu7UoqXqUOXNk7W2PU2j6eVwrpxqLbBAkBDQfHzPeq00ual6nU1J6NBuloiUs/+N7O/m7ZJH9pC90rNhOEHZsT2lasBaeM3IPDUoueh7cYSppWZIegUmfRh";
+    private AppCompatButton btn_webv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +55,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_str = findViewById(R.id.tv_str);
         tv_02 = findViewById(R.id.tv_02);
         tv_03 = findViewById(R.id.tv_03);
-        Button btn_en = findViewById(R.id.btn_en);
-        Button btn_de = findViewById(R.id.btn_de);
+        btn_webv2 = findViewById(R.id.btn_webv2);
+        AppCompatButton btn_en = findViewById(R.id.btn_en);
+        AppCompatButton btn_de = findViewById(R.id.btn_de);
         btn_en.setOnClickListener(this);
         btn_de.setOnClickListener(this);
+        btn_webv2.setOnClickListener(this);
         source = tv_str.getText().toString().trim();
 
     }
@@ -64,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            --------------- 此处模拟android端数据使用<服务器端公钥>加密后传到服务器--------------
             // 加密
             case R.id.btn_en:
+                Log.e("TAG","加密");
                 try {
                     // 从字符串中得到公钥
                     PublicKey publicKey = RSAUtils.loadPublicKey(PUCLIC_KEY);
@@ -82,6 +90,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            -----------------这里模拟服务器端人员拿到加密后的数据用 < 服务器私钥 > 解密---------- -
             // 解密
             case R.id.btn_de:
+                Log.e("TAG","解密");
+
                 String encryptContent = tv_02.getText().toString().trim();
                 try {
                     // 从字符串中得到私钥
@@ -96,6 +106,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                break;
+            case R.id.btn_webv2:
+                Log.e("TAG","跳转");
+
                 break;
             default:
                 break;
